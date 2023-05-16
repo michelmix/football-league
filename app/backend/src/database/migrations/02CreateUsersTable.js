01272be9ddc5168b09fa6a5fa9404ed7aa01111f
Michel-Barbosa <michelbarbosa@outlook.com>
2023-05-16 15:15:57 -0300

    Create Users migration

---
+++ app/backend/src/database/migrations/02CreateUsersTable.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
