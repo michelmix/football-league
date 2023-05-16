104d590aa34cc8c4ea5de8ff0dfb61f59d421c52
Michel-Barbosa <michelbarbosa@outlook.com>
2023-05-16 16:41:15 -0300

    Corrige testes de cobertura

--- app/backend/src/tests/Teams.test.ts
+++ app/backend/src/tests/Teams.test.ts
@@ -1,44 +1,51 @@
 import { expect } from "chai";
+import * as sinon from 'sinon';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import * as Sinon from "sinon";
 import ITeam from '../api/interfaces/ITeam'
 import { app } from "../app";
 import * as chai from 'chai';
-import { mock } from "node:test";
-// import Team from "../database/models/TeamModel";
+import Team from "../database/models/TeamsModel";
 // import { Model } from "sequelize";
 chai.use(chaiHttp)
 
+const mock: ITeam[] = [
+  {
+    "id": 1,
+    "teamName": "Avaí/Kindermann"
+  },
+  {
+    "id": 2,
+    "teamName": "Bahia"
+  },
+  {
+    "id": 3,
+    "teamName": "Botafogo"
+  },
+  {
+    "id": 4,
+    "teamName": "Corinthians"
+  },
+  {
+    "id": 5,
+    "teamName": "Cruzeiro"
+  },
+]
+
 describe('Test Service Teams', () => {
+  beforeEach(() => {
+    sinon
+      .stub(Team, "findAll")
+      .resolves(mock as unknown as Team[]);
+  });
   afterEach(function () {
     Sinon.restore()
   })
   // const app = new App()
 
   it('Retorna todos os times corretamente', async function () {
-    const mock: ITeam[] = [
-      {
-        "id": 1,
-        "teamName": "Avaí/Kindermann"
-      },
-      {
-        "id": 2,
-        "teamName": "Bahia"
-      },
-      {
-        "id": 3,
-        "teamName": "Botafogo"
-      },
-      {
-        "id": 4,
-        "teamName": "Corinthians"
-      },
-      {
-        "id": 5,
-        "teamName": "Cruzeiro"
-      },
-      ]
+
     const allTeams = await chai.request(app).get('/teams').send(mock);
     expect(allTeams.status).to.be.equal(200);
   })
@@ -53,13 +60,33 @@ describe('Test Service Teams', () => {
   });
 });
 
-describe('GET /teams/:id', async () => {
+describe('Testa a rota GET /teams/:id', async () => {
   it('Se responde com status 200 e um time', async () => {
+      sinon
+        .stub(Team, "findByPk")
+        .resolves(mock[1] as unknown as Team);
+
     const { body, status } = await chai
-      .request(app).get('/teams/1');
+      .request(app).get('/teams/2');
 
     expect(status).to.be.equal(200);
     expect(body).to.be.an('object');
-    expect(body).to.be.deep.equal(mock[0]);
-  })
-})
+    expect(body).to.be.deep.equal(mock[1]);
+  });
+
+  // it('Ao passar um id inválido response com status 404 e Team not found', async () => {
+  //   sinon
+  //     .stub(Team, "findByPk")
+  //     .resolves(null);
+
+  //   const { body, status } = await chai
+  //     .request(app).get('/teams/3');
+
+  //   expect(status).to.be.equal(404);
+  //   expect(body).to.be.an('object');
+  //   expect(body).to.be.deep.equal({ message: 'Team not found' });
+  // })
+  // afterEach(sinon.restore)
+});
+
+
