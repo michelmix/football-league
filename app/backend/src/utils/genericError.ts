9c78c2f9e1fe49cc0427fac23323a0b1522c73c6
Michel-Barbosa <michelbarbosa@outlook.com>
2023-05-19 18:41:22 -0300

    Deleta packages

--- app/backend/src/utils/genericError.ts
+++ app/backend/src/utils/genericError.ts
@@ -1,15 +1,15 @@
-import { NextFunction, Request, Response } from 'express';
-import { TokenExpiredError, JsonWebTokenError } from 'jsonwebtoken';
-import ApiError from './Error';
+// import { NextFunction, Request, Response } from 'express';
+// import { TokenExpiredError, JsonWebTokenError } from 'jsonwebtoken';
+// import ApiError from './Error';
 
-export default (error: ApiError | Error, _req: Request, res: Response, _Next: NextFunction) => {
-  if (error instanceof ApiError) {
-    return res.status(error.status).json({ message: error.message });
-  }
+// export default (error: ApiError | Error, _req: Request, res: Response, _Next: NextFunction) => {
+//   if (error instanceof ApiError) {
+//     return res.status(error.status).json({ message: error.message });
+//   }
 
-  if (error instanceof TokenExpiredError || error instanceof JsonWebTokenError) {
-    return res.status(401).json({ message: 'Token must be a valid token' });
-  }
+//   if (error instanceof TokenExpiredError || error instanceof JsonWebTokenError) {
+//     return res.status(401).json({ message: 'Token must be a valid token' });
+//   }
 
-  return res.status(500).json({ message: 'Server error' });
-};
+//   return res.status(500).json({ message: 'Server error' });
+// };
