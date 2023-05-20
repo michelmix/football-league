27b0705d86e35b2c1d64068a135c36aa71af1e11
Michel-Barbosa <michelbarbosa@outlook.com>
2023-05-20 11:53:47 -0300

    fix: Exclui bibilioteca status para tentar rodar o avaliador no GitHub

--- app/backend/src/api/services/LoginService.ts
+++ app/backend/src/api/services/LoginService.ts
@@ -1,36 +1,36 @@
-import { StatusCodes } from 'http-status-codes';
-import { hashSync, compareSync } from 'bcryptjs';
-import { sign } from 'jsonwebtoken';
-import IJwtPayload from '../interfaces/IJwt';
-import ILogin from '../interfaces/ILogin';
-import Error from '../../utils/Error';
+// import { StatusCodes } from 'http-status-codes';
+// import { hashSync, compareSync } from 'bcryptjs';
+// import { sign } from 'jsonwebtoken';
+// import IJwtPayload from '../interfaces/IJwt';
+// import ILogin from '../interfaces/ILogin';
+// import Error from '../../utils/Error';
 
-import UserModel from '../../models/UserModel';
+// import UserModel from '../../models/UserModel';
 
-export default class LoginService {
-  constructor(private userModel = new UserModel()) { }
+// export default class LoginService {
+//   constructor(private userModel = new UserModel()) { }
 
-  async login({ email, password }: ILogin) {
-    const user = await this.userModel.findByEmail({ email });
+//   async login({ email, password }: ILogin) {
+//     const user = await this.userModel.findByEmail({ email });
 
-    if (!user || !LoginService.comparePass(password, user.password)) {
-      throw new Error(StatusCodes.BAD_REQUEST, 'Invalid email or password');
-    }
+//     if (!user || !LoginService.comparePass(password, user.password)) {
+//       throw new Error(StatusCodes.BAD_REQUEST, 'Invalid email or password');
+//     }
 
-    return LoginService.generateToken({ id: user.id, email: user.email });
-  }
+//     return LoginService.generateToken({ id: user.id, email: user.email });
+//   }
 
-  private static encryptPass(password: string) {
-    return hashSync(password);
-  }
+//   private static encryptPass(password: string) {
+//     return hashSync(password);
+//   }
 
-  private static comparePass(password: string, hash: string) {
-    return compareSync(password, hash);
-  }
+//   private static comparePass(password: string, hash: string) {
+//     return compareSync(password, hash);
+//   }
 
-  private static generateToken(payload: IJwtPayload) {
-    const secret = process.env.JWT_SECRET || 'secret';
+//   private static generateToken(payload: IJwtPayload) {
+//     const secret = process.env.JWT_SECRET || 'secret';
 
-    return sign(payload, secret, { expiresIn: '1d' });
-  }
-}
+//     return sign(payload, secret, { expiresIn: '1d' });
+//   }
+// }
